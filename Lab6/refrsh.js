a4eb4f19466806269f637f08f9522466ab194a9a
jpmaatz <97722319+jpmaatz@users.noreply.github.com>
2024-05-16 17:29:55 -0500

    Add files via upload

---
+++ Lab6/refrsh.js
@@ -0,0 +1,21 @@
+const firstName = "Jordan";
+const lastName = "Maatz";
+
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 3000;
+
+console.log(population);
+console.log(population * 3);
+
+const javaScriptIsCool = true;
+
+for (a = 0; a < 6; a++){
+    console.log(a);
+}
+
+for (b = 0; b < 5; b++){
+    console.log(b);
+}
