82160452fac8eb103ed974459fb821f9b276e50e
jpmaatz <97722319+jpmaatz@users.noreply.github.com>
2024-07-08 17:33:13 -0500

    Add files via upload
    piglatin

---
+++ piglatin/prac.test.js
@@ -0,0 +1,15 @@
+
+const {
+	pigLatinWords
+} = require("./prac")
+
+describe('pigLatinWords'), () => {
+	it("adding 'way' and the first consonent to the end of the word"), () => {
+		const vowels = "aeiou"
+		let index = 0
+		const result = ""
+
+		pigLatinWords(word)
+		expect(word).toEqual(result)
+	}
+}
